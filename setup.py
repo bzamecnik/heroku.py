e0296bb978c3ba65e9632221ffab5f81bfc49bf9
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2015-05-24 12:34:01 +0200

    Update setup.py with Python 3 compatibility.

--- setup.py
+++ setup.py
@@ -44,5 +44,6 @@ setup(
         'Programming Language :: Python :: 2.7',
         # 'Programming Language :: Python :: 3.0',
         # 'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.4',
     ),
 )
